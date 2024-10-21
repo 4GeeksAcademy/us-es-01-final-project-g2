e3abca2986bbea7b44e2df39dbcda9fdeb996857
kleinercalle <kleiner2024@gmail.com>
2024-10-21 23:55:06 +0000

    [models] feat: users

--- src/api/models.py
+++ src/api/models.py
@@ -1,14 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 
-
+# Modelo de Usuario (Users)
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    bio = db.Column(db.String(255))
+    profile_picture = db.Column(db.String(255))
+    birthdate = db.Column(db.Integer)  # Almacena año de nacimiento como número
+    is_active = db.Column(db.Boolean(), nullable=False, default=True)
+    created_at = db.Column(db.DateTime, nullable=False )
+    updated_at = db.Column(db.DateTime, nullable=False )
 
     def __repr__(self):
         return f'<User {self.id} - {self.email}>'
@@ -16,4 +20,9 @@ class Users(db.Model):
     def serialize(self):
         return {'id': self.id,
                 'email': self.email,
-                'is_active': self.is_active}
+                'bio': self.bio,
+                'profile_picture': self.profile_picture,
+                'birthdate': self.birthdate,
+                'is_active': self.is_active,
+                'created_at': self.created_at,
+                'updated_at': self.updated_at}
